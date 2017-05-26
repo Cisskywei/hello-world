17d56e446e1a9442319a45248b9e5d2dfff38280
Lvv <1457511196@qq.com>
2017-05-26 13:34:58 +0800

    更新客户端

---
+++ developClass/Assets/Scripts/Common/Enums.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enums {
+
+	public enum SexEnum
+    {
+        None = 0,
+        Male,
+        Female,
+    }
+
+    public enum DutyEnum
+    {
+        None = 0,
+        Student,
+        GroupLeader,
+        Teacher,
+    }
+}
---
+++ developClass/Assets/Scripts/UI/Data/PlayerBaseInfor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBaseInfor {
+
+    public string name;
+    public Enums.SexEnum sex = Enums.SexEnum.None;
+    public Enums.DutyEnum duty = Enums.DutyEnum.None;
+}
