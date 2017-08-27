b8600b9bd9714b7a3ceb929b1926cb8c0ddd8762
Cisskywei <1457511196@qq.com>
2017-08-27 22:57:54 +0800

    该域名

--- vrClass/veClassRoom/veClassRoom/Room/BackServer/BackServerConfig.cs
+++ vrClass/veClassRoom/veClassRoom/Room/BackServer/BackServerConfig.cs
@@ -8,7 +8,8 @@ namespace veClassRoom.Room
 {
     class BackServerConfig
     {
-        public static readonly string HD_Url = "http://www.hdmooc.com:5557";
+        //public static readonly string HD_Url = "http://www.hdmooc.com:5557";
         //public static readonly string HD_Url = "http://www.coolthinking.cn:8000";
+        public static readonly string HD_Url = "http://www.hdmool.com:8000";
     }
 }
